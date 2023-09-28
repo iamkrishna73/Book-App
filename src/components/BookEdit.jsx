a76e2fa2d6648f55999a752e7ef70d92433b927f
krishna <krishnasingj137333@gmail.com>
2023-09-28 22:12:04 +0530

    initial commit

---
+++ src/components/BookEdit.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const BookEdit = ({book, onSubmit}) => {
+  const [title, setTitle] = useState(book.title);
+  
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(book.id, title);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="book-edit">
+        <label htmlFor=''>Title</label>
+        <input type="text" value={title} onChange={handleChange} className="input"/>
+        <button className="button is-primary">Save</button>
+      </form>
+    </div>
+  );
+};
+
+export default BookEdit;
